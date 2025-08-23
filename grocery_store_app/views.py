4007ff3756764dea84a855aff0f8215d46463537
conorll <141634178+conorll@users.noreply.github.com>
2025-08-23 15:15:15 +1000

    create products page

--- grocery_store_app/views.py
+++ grocery_store_app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+from .models import Product
+
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
   return render(request, "grocery_store_app/index.html")
+
+def products(request):
+  product_objects = Product.objects.all()
+  return render(request, "grocery_store_app/products.html", {
+    "products": product_objects
+  })
