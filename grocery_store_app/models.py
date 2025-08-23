96308b175930ec0dec654c5b11e9b1a93dc0ef51
conorll <141634178+conorll@users.noreply.github.com>
2025-08-23 12:15:40 +1000

    create category and product models

--- grocery_store_app/models.py
+++ grocery_store_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+  name = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name
+
+  
+class Product(models.Model):
+  name = models.CharField(max_length=50)
+  description = models.CharField(max_length=500)
+  price = models.DecimalField(max_digits=10, decimal_places= 2)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+  quantity = models.PositiveIntegerField()
+
+  def __str__(self):
+    return self.name
