bf5a6c5f51bac07f91023db14eadb8ea5cf6927d
conorll <141634178+conorll@users.noreply.github.com>
2025-08-23 12:38:11 +1000

    install grocery store app + create index page

--- grocery_store/settings.py
+++ grocery_store/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'grocery_store_app.apps.GroceryStoreAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
