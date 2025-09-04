1e6cda8cba4139f71e673f59550dc654ae2dbaa0
Seb Joubert <87304337+Sebjou@users.noreply.github.com>
2025-09-04 13:08:29 +1000

    Update utils.py

--- grocery_store_app/utils.py
+++ grocery_store_app/utils.py
@@ -24,7 +24,7 @@ def geocode_postcode(postcode: str) -> Tuple[float, float] | Tuple[None, None]:
     return None, None
 
 
-# Haversine formula to calculate distance between two lat/lon points
+# Haversine formula to calculate distance between two lat/lon point
 def haversine(lat1, lon1, lat2, lon2):
     R = 6371  # Earth radius in km
     phi1, phi2 = math.radians(lat1), math.radians(lat2)
