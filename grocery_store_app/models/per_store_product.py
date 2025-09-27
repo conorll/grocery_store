c56417fceab502a2b0203a5fb4ea7118e96e7fff
conorll <141634178+conorll@users.noreply.github.com>
2025-09-27 17:02:25 +1000

    add per_store_product model and remove quantity field from product model

---
+++ grocery_store_app/models/per_store_product.py
@@ -0,0 +1,11 @@
+from django.db import models
+from .product import Product 
+from .store import Store 
+
+class PerStoreProduct(models.Model):
+  product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="per_store_products")
+  store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name="per_store_products")
+  quantity = models.PositiveIntegerField()
+
+  def __str__(self):
+    return self.name
--- grocery_store_app/models/product.py
+++ grocery_store_app/models/product.py
@@ -5,7 +5,6 @@ class Product(models.Model):
   name = models.CharField(max_length=50)
   price = models.DecimalField(max_digits=10, decimal_places= 2)
   category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
-  quantity = models.PositiveIntegerField()
   image_url = models.CharField(max_length=1000)
 
   def __str__(self):
