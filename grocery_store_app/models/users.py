287a7cdc1e5d64523a7b3e28238167164802143c
Lilys-Code <lilybwatt@outlook.com>
2025-09-30 19:07:15 +1000

    Added associated store to user

---
+++ grocery_store_app/models/users.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractUser
+
+from .store import *
+
+class CustomUser(AbstractUser):
+    store = models.ForeignKey(Store, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        db_table = 'auth_user'
+
