2613d13dae6c395362b2f06cd01ffd637add1184
Seb <seabassjou@gmail.com>
2025-10-06 23:02:35 +1100

    List of stores automated testing

---
+++ grocery_store_app/tests/test_list_of_stores.py
@@ -0,0 +1,37 @@
+from unittest.mock import patch
+from django.test import TestCase, Client
+from grocery_store_app.models import Store
+
+class ListOfStoresTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        # Create two stores with known locations
+        self.store_a = Store.objects.create(
+            name="Store A",
+            address="100 Main St",
+            postcode="2000",
+            phone_number="123456789",
+            opening_hours="9am - 5pm",
+            latitude=-33.8675,
+            longitude=151.2070
+        )
+        self.store_b = Store.objects.create(
+            name="Store B",
+            address="200 Side St",
+            postcode="3000",
+            phone_number="987654321",
+            opening_hours="10am - 6pm",
+            latitude=-37.814,
+            longitude=144.96332
+        )
+
+    @patch("grocery_store_app.views.geocode_postcode")
+    def test_closest_store_geolocation(self, mock_geocode):
+        # Mock the coordinates returned from postcode geocoding
+        mock_geocode.return_value = (-33.86, 151.20)  # Near Sydney
+
+        response = self.client.post("/stores", data={"postcode": "2000"})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Your closest store is")
+        self.assertContains(response, "Store A")
